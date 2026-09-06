087f978ca8277b5fb97947245b9736a27c653e85
roshinird <roshinird33@gmail.com>
2026-09-06 11:19:06 +0530

    Add smooth ambulance movement

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -1,5 +1,12 @@
+import { useEffect, useState } from 'react'
 import L from 'leaflet'
-import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet'
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Polyline,
+  Popup,
+} from 'react-leaflet'
 import { routeData } from '../data'
 
 const ambulanceIcon = L.divIcon({
@@ -17,7 +24,58 @@ const hospitalIcon = L.divIcon({
 })
 
 function MapView({ route, traffic }) {
-  const ambulancePosition = routeData.ambulance.position
+  const [ambulancePosition, setAmbulancePosition] = useState(route[0])
+
+  useEffect(() => {
+    if (!route || route.length < 2) {
+      return
+    }
+
+    let segmentIndex = 0
+    let animationFrame
+    let segmentStartTime = null
+
+    const segmentDuration = 4000
+
+    const animate = (timestamp) => {
+      if (!segmentStartTime) {
+        segmentStartTime = timestamp
+      }
+
+      const elapsed = timestamp - segmentStartTime
+      const progress = Math.min(elapsed / segmentDuration, 1)
+
+      const start = route[segmentIndex]
+      const end = route[segmentIndex + 1]
+
+      const latitude =
+        start[0] + (end[0] - start[0]) * progress
+
+      const longitude =
+        start[1] + (end[1] - start[1]) * progress
+
+      setAmbulancePosition([latitude, longitude])
+
+      if (progress < 1) {
+        animationFrame = requestAnimationFrame(animate)
+      } else {
+        segmentIndex += 1
+        segmentStartTime = null
+
+        if (segmentIndex < route.length - 1) {
+          animationFrame = requestAnimationFrame(animate)
+        }
+      }
+    }
+
+    setAmbulancePosition(route[0])
+    animationFrame = requestAnimationFrame(animate)
+
+    return () => {
+      cancelAnimationFrame(animationFrame)
+    }
+  }, [route])
+
   const hospitalPosition = routeData.hospital.position
 
   const trafficColors = {
@@ -30,24 +88,36 @@ function MapView({ route, traffic }) {
 
   return (
     <MapContainer
-      center={ambulancePosition}
+      center={routeData.ambulance.position}
       zoom={13}
       style={{ height: '100%', width: '100%' }}
     >
       <TileLayer
-        attribution='&copy; OpenStreetMap contributors'
+        attribution="&copy; OpenStreetMap contributors"
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
       <Marker
         position={ambulancePosition}
         icon={ambulanceIcon}
-      />
+      >
+        <Popup>
+          🚑 <strong>Ambulance</strong>
+          <br />
+          Current Location
+        </Popup>
+      </Marker>
 
       <Marker
         position={hospitalPosition}
         icon={hospitalIcon}
-      />
+      >
+        <Popup>
+          🏥 <strong>Hospital</strong>
+          <br />
+          Destination
+        </Popup>
+      </Marker>
 
       <Polyline
         positions={route}
